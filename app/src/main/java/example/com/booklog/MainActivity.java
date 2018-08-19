aea7fb36e97e39d0e36bdf7695cb2f1ac8b740bb
mahima <mahima.6.ms@gmail.com>
2018-08-19 19:35:54 +0530

    add display data method to read data from database and display in a textview

--- app/src/main/java/example/com/booklog/MainActivity.java
+++ app/src/main/java/example/com/booklog/MainActivity.java
@@ -1,11 +1,13 @@
 package example.com.booklog;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import static example.com.booklog.BookContract.BookEntry.COLUMN_NAME;
 import static example.com.booklog.BookContract.BookEntry.COLUMN_PRICE;
@@ -16,27 +18,55 @@ import static example.com.booklog.BookContract.BookEntry.TABLE_NAME;
 
 public class MainActivity extends AppCompatActivity {
 
-    BookDbHelper dbHelper;
-    SQLiteDatabase database;
+    private BookDbHelper dbHelper;
+    private SQLiteDatabase database;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dbHelper = new BookDbHelper(this);
-        database = dbHelper.getWritableDatabase();
+        textView = findViewById(R.id.dataTextView);
 
-        insertDummyData();
+        dbHelper = new BookDbHelper(this);
+
+        Cursor cursor = fetchData();
+
+        displayData(cursor);
+    }
+
+    private void displayData(Cursor cursor) {
+        if (cursor != null && cursor.getCount() > 0) {
+            StringBuilder stringBuilder = new StringBuilder();
+            while (cursor.moveToNext()) {
+                stringBuilder.append("\n\nBook Name: ").append(cursor.getString(cursor.getColumnIndex(COLUMN_NAME))).append("\nPrice : ").append(cursor.getDouble(cursor.getColumnIndex(COLUMN_PRICE))).append("\nQuantity : ").append(cursor.getInt(cursor.getColumnIndex(COLUMN_QUANTITY)));
+            }
+            textView.setText(stringBuilder.toString());
+        }
+        if (cursor != null) {
+            cursor.close();
+        }
+    }
+
+    private Cursor fetchData() {
+
+        database = dbHelper.getReadableDatabase();
+
+        String[] columns = {COLUMN_NAME, COLUMN_PRICE, COLUMN_QUANTITY};
+        return database.query(TABLE_NAME, columns, null, null, null, null, null);
     }
 
     private void insertDummyData() {
+        database = dbHelper.getWritableDatabase();
+
         ContentValues contentValues = new ContentValues();
         contentValues.put(COLUMN_NAME, "How to kill a mocking bird");
         contentValues.put(COLUMN_PRICE, 399);
         contentValues.put(COLUMN_QUANTITY, 3);
         contentValues.put(COLUMN_SUPPLIER_NAME, "Arrow books");
         contentValues.put(COLUMN_SUPPLIER_PHONE, "998-(243)-3217");
+
         database.insert(TABLE_NAME, null, contentValues);
     }
 
