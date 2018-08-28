a22fed0dfb94b56861584ee8b96e628cbe471e0a
Mahima <mahima.6.ms@gmail.com>
2018-08-28 10:04:59 +0530

    add content provider constants
    define custom content provider class

--- app/src/main/java/example/com/booklog/BookContract.java
+++ app/src/main/java/example/com/booklog/BookContract.java
@@ -13,6 +13,11 @@ public class BookContract {
         public static final String COLUMN_QUANTITY = "quantity";
         public static final String COLUMN_SUPPLIER_NAME = "supplier_name";
         public static final String COLUMN_SUPPLIER_PHONE = "supplier_phone";
+
+        public static final String CONTENT_AUTHORITY = BuildConfig.APPLICATION_ID;
+        public static final String BASE_URI = "content://" + CONTENT_AUTHORITY;
+        public static final String CONTENT_URI = BASE_URI + "/" + TABLE_NAME;
+        public static final String CONTENT_URI_ID = BASE_URI + "/" + TABLE_NAME + "/#";
         
     }
 }
